9247e68d1423fab5b620f82279b7abfbc92ef726
Christoph <c.chmela@gmail.com>
2020-09-10 23:27:50 +0200

    Add one demo/test controller.

---
+++ src/main/java/cc/developement/tabletennis/controller/DemoController.java
@@ -0,0 +1,15 @@
+package cc.developement.tabletennis.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	
+	@GetMapping
+	public String index() {
+		return "Hallo beim Tischtennis";
+	}
+	
+}
